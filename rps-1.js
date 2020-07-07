482191ccb504a0a7d33e2a8b7255ba55ba550800
stephtyc <windyhill722@gmail.com>
2020-07-07 23:32:38 +0800

    add dom to script

--- rps-1.js
+++ rps-1.js
@@ -1,7 +1,8 @@
-let result;
 let playerWin = 0;
 let computerWin = 0;
 let playCount = 0;
+
+//Computer choice.
 
 function computerPlay() {
 	let computerChoice = Math.floor(Math.random() * 3);
@@ -14,65 +15,140 @@ function computerPlay() {
 	}
 }
 
-function playRound(playerSelection, computerSelection) {
-	playerChoice = playerSelection.toLowerCase();
+//When it shows "it's a draw" how do I remove the results in the next round? stick an event listener to one of the buttons?
+const btnRock = document.querySelector("#rock");
+btnRock.addEventListener("click", function () {
+	if (playCount < 5) {
+		let playerChoice = "rock";
+		let computerChoice = computerPlay();
+		playRound(playerChoice, computerChoice);
+		playCount++;
+	} else if (playCount >= 5) {
+		alert("Click reset button to play again!");
+	}
+	// if (playCount >= 5) {
+	// 	alert("Click reset button to play again!");
+	// 	return;
+	// } else {
+	// 	let playerChoice = "rock";
+	// 	let computerChoice = computerPlay();
+	// 	playRound(playerChoice, computerChoice);
+	// 	playCount++;
+	// }
+	//Once 5 rounds are finished, show results and reset button
+	if (playCount === 5) {
+		totalScore(playerWin, computerWin);
+	}
+});
 
+const btnPaper = document.querySelector("#paper");
+btnPaper.addEventListener("click", () => {
+	if (playCount >= 5) {
+		alert("Click reset button to play again!");
+	} else {
+		let playerChoice = "rock";
+		let computerChoice = computerPlay();
+		playRound(playerChoice, computerChoice);
+		playCount++;
+	}
+
+	if (playCount === 5) {
+		totalScore(playerWin, computerWin);
+	}
+});
+
+const btnScissor = document.querySelector("#scissor");
+btnScissor.addEventListener("click", () => {
+	if (playCount >= 5) {
+		alert("Click reset button to play again!");
+	} else {
+		let playerChoice = "scissor";
+		let computerChoice = computerPlay();
+		playRound(playerChoice, computerChoice);
+		playCount++;
+	}
+
+	if (playCount === 5) {
+		totalScore(playerWin, computerWin);
+	}
+});
+
+function playRound(playerChoice, computerChoice) {
 	if (
-		(playerChoice === "rock" && computerSelection === "scissor") ||
-		(playerChoice === "paper" && computerSelection === "rock") ||
-		(playerChoice === "scissor" && computerSelection === "paper")
+		(playerChoice === "rock" && computerChoice === "scissor") ||
+		(playerChoice === "paper" && computerChoice === "rock") ||
+		(playerChoice === "scissor" && computerChoice === "paper")
 	) {
+		playerWin++;
+		scoreBoard();
+		//checking if the number of wins are being counted properly
+
+		console.log(`Player wins ${playerWin} rounds`);
 		return (result = "win");
 	} else if (
-		(playerChoice === "scissor" && computerSelection === "rock") ||
-		(playerChoice === "rock" && computerSelection === "paper") ||
-		(playerChoice === "paper" && computerSelection === "scissor")
+		(playerChoice === "scissor" && computerChoice === "rock") ||
+		(playerChoice === "rock" && computerChoice === "paper") ||
+		(playerChoice === "paper" && computerChoice === "scissor")
 	) {
+		computerWin++;
+		scoreBoard();
+		console.log(`Computer wins ${computerWin} rounds`);
 		return (result = "lose");
 	} else if (
-		(playerChoice === "scissor" && computerSelection === "scissor") ||
-		(playerChoice === "rock" && computerSelection === "rock") ||
-		(playerChoice === "paper" && computerSelection === "paper")
+		(playerChoice === "scissor" && computerChoice === "scissor") ||
+		(playerChoice === "rock" && computerChoice === "rock") ||
+		(playerChoice === "paper" && computerChoice === "paper")
 	) {
+		console.log(`It's a draw! ${playCount} rounds were played`);
+		let draw_score = document.createElement("h4");
+		draw_score.textContent = "It's a draw!";
+		document.body.appendChild(draw_score);
+
+		scoreBoard();
+
 		return (result = "draw");
-	}
+	} //to remove the "it's a draw" once user presses another selection. if statement, conditions use playCoount> 5?
 }
 
-function game() {
-	for (i = 0; i < 5; i++) {
-		computerChoice = computerPlay();
-		let playerChoice = prompt("Rock, paper or scissors?");
-		console.log(
-			`Computer chose ${computerChoice}, Player chose ${playerChoice}`
-		);
-		playRound(playerChoice, computerChoice);
-
-		if (result == "win") {
-			playerWin++;
-			playCount++;
-			console.log(
-				`You win! ${playerChoice} beats ${computerChoice} in Round ${playCount}`
-			);
-		} else if (result == "lose") {
-			computerWin++;
-			playCount++;
-			console.log(
-				`You lose! ${computerChoice} beats ${playerChoice} in Round ${playCount}`
-			);
-		} else if (result == "draw") {
-			playCount++;
-			console.log(`It\'s a draw in Round ${playCount}`);
-		}
-	}
-	console.log("--------");
-	//Total Results
+function totalScore(playerWin, computerWin) {
 	if (playerWin > computerWin) {
-		console.log(`You win with ${playerWin} rounds`);
+		let scoreAnnouncement = document.createElement("h4");
+		scoreAnnouncement.textContent = "Player won!";
+		scoreAnnouncement.style.cssText = "color: #2A52BE";
+		document.body.appendChild(scoreAnnouncement);
 	} else if (playerWin < computerWin) {
-		console.log(`Computer wins with ${computerWin} rounds`);
-	} else if (playerWin === computerWin) {
-		console.log(`It\'s a tie`);
+		let scoreAnnouncement = document.createElement("h4");
+		scoreAnnouncement.textContent = "Computer won!";
+		scoreAnnouncement.style.cssText = "color: #2A52BE";
+
+		document.body.appendChild(scoreAnnouncement);
+	} else {
+		let scoreAnnouncement = document.createElement("h4");
+		scoreAnnouncement.textContent = "It's a draw!";
+		scoreAnnouncement.style.cssText = "color: #2A52BE";
+
+		document.body.appendChild(scoreAnnouncement);
 	}
+
+	const reset_btn = document.createElement("button");
+	reset_btn.setAttribute("class", "reset");
+	reset_btn.textContent = "Let's Play Again!";
+	reset_btn.addEventListener("click", reloadPage);
+	document.body.appendChild(reset_btn);
 }
 
-game();
+//Sync up win count with the HTML scoreboard section
+function scoreBoard() {
+	let user_score = document.getElementById("user-score");
+	let comp_score = document.getElementById("comp-score");
+
+	user_score.innerHTML = playerWin;
+
+	comp_score.innerHTML = computerWin;
+}
+
+//For resetting the game when the user clicks the reset button or when either user or computer wins
+
+function reloadPage() {
+	window.location.reload();
+}
